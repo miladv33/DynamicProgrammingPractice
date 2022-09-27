c693c5061c6e3867689eed9e5be6945dc11aca75
Mousavi <miladv33@gmail.com>
2022-09-27 12:36:46 +0330

    find miimum size of subarray

--- app/src/main/java/com/example/dynamicprogrammingpractice/slidingwindow/MinimumSizeSubarraySum.kt
+++ app/src/main/java/com/example/dynamicprogrammingpractice/slidingwindow/MinimumSizeSubarraySum.kt
@@ -8,22 +8,62 @@ import kotlin.math.min
 
 fun main() {
     val intArray2 = intArrayOf(2, 3, 1, 2, 4, 3)
-    println(minSubArrayLen(7, intArray2)) // 2
-    println(minSubArrayLen(11, intArrayOf(1, 1, 1, 1, 1, 1, 1, 1))) // 0
-    println(minSubArrayLen(15, intArrayOf(1, 2, 3, 4, 5))) // 5
+    println(solutionForMinSubArrayLen(7, intArray2)) // 2
+    println(solutionForMinSubArrayLen(11, intArrayOf(1, 1, 1, 1, 1, 1, 1, 1))) // 0
+    println(solutionForMinSubArrayLen(15, intArrayOf(1, 2, 3, 4, 5))) // 5
 }
 
+/**
+ * We start with an empty subarray, and add elements to it until the sum of the subarray is greater
+ * than or equal to the target. Then we drop elements from the subarray until the sum is less than the
+ * target. We keep track of the minimum length of the subarray that satisfies the condition
+ *
+ * @param target the target sum
+ * @param nums [2,3,1,2,4,3]
+ * @return The minimum length of the subarray that sums to the target.
+ */
 fun minSubArrayLen(target: Int, nums: IntArray): Int {
     var subArray = intArrayOf()
     var minLengthOfSum = nums.sum() + 1
+    var numberOfDrop = 0
     for (i in nums.indices) {
         subArray += nums[i]
-        while (subArray.sum() >= target) {
-            if (minLengthOfSum >= subArray.size) {
-                minLengthOfSum = subArray.size
+        var tempSize = subArray.sum()
+        if (tempSize >= target) {
+            while (subArray.sum() >= target) {
+                if (minLengthOfSum >= subArray.size) {
+                    minLengthOfSum = subArray.size
+                }
+                numberOfDrop++
+                subArray = subArray.drop(1).toIntArray()
             }
-            subArray = subArray.drop(1).toIntArray()
         }
     }
     return if (minLengthOfSum > nums.size) 0 else minLengthOfSum
+}
+
+
+/**
+ * We use a sliding window, with left and right pointers. We add elements from the right pointer and
+ * subtract elements from the left pointer to get the sum within the window. If the sum is greater than
+ * the target, we remove elements from the left pointer to decrease the sum. We keep doing this until
+ * the sum is less than the target
+ *
+ * @param target the target value we're looking for
+ * @param nums the array of integers
+ * @return The minimum length of a contiguous subarray of which the sum ≥ s. If there isn't one, return
+ * 0 instead.
+ */
+fun solutionForMinSubArrayLen(target: Int, nums: IntArray): Int {
+    var minesLengthOfSubArray = Int.MAX_VALUE
+    var left = 0
+    var sum = 0
+    for (i in nums.indices) {
+        sum += nums[i]
+        while (sum >= target) {
+            minesLengthOfSubArray = min(minesLengthOfSubArray, i + 1 - left)
+            sum -= nums[left++]
+        }
+    }
+    return if (minesLengthOfSubArray != Int.MAX_VALUE) minesLengthOfSubArray else 0
 }
--- app/src/main/java/com/example/dynamicprogrammingpractice/slidingwindow/longestString.kt
+++ app/src/main/java/com/example/dynamicprogrammingpractice/slidingwindow/longestString.kt
@@ -3,14 +3,31 @@ package com.example.dynamicprogrammingpractice.slidingwindow
 import androidx.compose.runtime.currentComposer
 
 fun main() {
-//    var string = "aabacbebebe"
-//    var string = "aabbcc"
-    var string = "abcabcbb"
-//    var string = "abcdss"
-    longestDistinctSubstring(string, 2)
+    var string1 = "aabacbebebe"
+    var string2 = "aabbcc"
+    var string3 = "abcabcbb"
+    var string4 = "abcdss"
+    println(longestDistinctSubstring(string1, 2)) // bebebe
+    println(longestDistinctSubstring(string2, 2)) //
+    println(longestDistinctSubstring(string3, 2)) //
+    println(longestDistinctSubstring(string4, 2)) //
+    println("-------------")
+    println(betterLongestDistinctSubstring(string1, 2)) // bcbb
+    println(betterLongestDistinctSubstring(string2, 2)) // bcbb
+    println(betterLongestDistinctSubstring(string3, 2)) // bcbb
+    println(betterLongestDistinctSubstring(string4, 2)) // bcbb
 }
 
-fun longestDistinctSubstring(string: String, k: Int) {
+
+/**
+ * It iterates through the string, adding each character to a substring, and if the substring has more
+ * than k distinct characters, it drops the first character of the substring
+ *
+ * @param string The string to search for the longest substring in.
+ * @param k the number of distinct characters in the substring
+ * @return The longest substring of the input string that contains no more than k distinct characters.
+ */
+fun longestDistinctSubstring(string: String, k: Int): String {
     var substring = ""
     for (i in string.indices) {
         substring += string[i]
@@ -18,7 +35,24 @@ fun longestDistinctSubstring(string: String, k: Int) {
             substring = substring.drop(1)
         }
     }
-    println(substring.length)
-    println(substring)
+    return substring
+}
+
+/**
+ * If the substring from the pointer to the current index has more than k distinct characters,
+ * increment the pointer.
+ *
+ * @param string The string to find the longest substring of.
+ * @param k the number of distinct characters allowed in the substring
+ * @return The longest substring of the string that has at most k distinct characters.
+ */
+fun betterLongestDistinctSubstring(string: String, k: Int): String {
+    var pointer = 0
+    for (i in string.indices) {
+        if (string.substring(pointer, i+1).toCharArray().distinct().size > k) {
+            pointer++
+        }
+    }
+    return string.substring(pointer)
 }
 
